441457f934e9ee9fd9d70ec775fbed2c9d425124
Deathmatix <129149375+Deathmatix@users.noreply.github.com>
2023-10-12 22:09:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,59 +7,58 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from pyrogram.enums import ParseMode
 
 moderator_ids = [1902879847, 1097234204, 5543537764]
-api_id = 13340341
-api_hash = "e83570d934a86b99cc9bdd3210c1d269"
-bot_token = "6628987065:AAHhoQxS1ZVX35I2dv8CwaKlv32f9DESGZw"
+api_id = 29248158
+api_hash = "75bb1a0bbeb8435a7904321df8782c8b"
+bot_token = "6582993584:AAEwCvVXuDTAwcx7n8bjBdHxgiVwbX0wmvo"
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 user_choices = {}
 
 def get_github_raw_file_content_as_list(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        content_list = response.text.split("\n")
-        user_list = []
-        for line in content_list:
-            line = line.strip() 
-            if "#" in line:
-                line = line.split("#")[0].strip()
-            user_list.append(line)
-        return user_list
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return []
+    response = requests.get(url)
+    response.raise_for_status()
+    content_list = response.text.split("\n")
+    user_list = []
+    for line in content_list:
+        line = line.strip() 
+        if "#" in line:
+            line = line.split("#")[0].strip()
+        user_list.append(line)
+    return user_list
 
 def is_user_registered(user_id):
     with open("users.txt", "r") as user_file:
         user_list = user_file.read().splitlines()
     return str(user_id) in user_list
 
+def load_user_usage():
+    with open("user_usage.json", "r") as file:
+        return json.load(file)
+
+def save_user_usage(user_usage):
+    with open("user_usage.json", "w") as file:
+        json.dump(user_usage, file)
+
 @app.on_message(filters.command('start'))
 async def start(client, message):
     if not is_user_registered(message.from_user.id):
         with open("users.txt", "a") as user_file:
             user_file.write(str(message.from_user.id) + "\n")
+        await message.reply_text("**Welcome! You are a free user. You get 2 free searches.**", parse_mode=ParseMode.MARKDOWN)
     search_button = InlineKeyboardButton("Search", callback_data="search_options")
     keyboard = InlineKeyboardMarkup([[search_button]])
-    await message.reply_text("**Welcome! This bot helps by searching logs database and find data you ask.**\n"
+    await message.reply_text("**This bot helps by searching logs database and find data you ask.**\n"
                             "**Made by - @Deathmatix \n\nTo search for something, simply click the 'Search' button below.**", reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
 
 @app.on_message(filters.command('search'))
 async def search_command(client, message):
-    url = "https://raw.githubusercontent.com/Deathmatix/verify-logsearch/main/users.txt"
-    response = requests.get(url)
-    user_list = response.text.split("\n")
-    if str(message.from_user.id) in user_list:
-        search_button = InlineKeyboardButton("Search", callback_data="search_options")
-        keyboard = InlineKeyboardMarkup([[search_button]])
-        await message.reply_text("**To search for something, simply click the 'Search' button below.**", reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
-    else:
-        await message.reply_text("**You are not a registered user. Contact @deathmatix to get the subscription.**", parse_mode=ParseMode.MARKDOWN)
+    search_button = InlineKeyboardButton("Search", callback_data="search_options")
+    keyboard = InlineKeyboardMarkup([[search_button]])
+    await message.reply_text("**To search for something, simply click the 'Search' button below.**", reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
 
 @app.on_message(filters.command('id'))
 async def id_command(client, message):
     user_id = message.from_user.id
-    await message.reply_text(f"**Your User ID:**\n`{user_id}`\n\n**You can click to copy and send it to @deathmatix if you want to buy a subscription.**", parse_mode=ParseMode.MARKDOWN)
+    await message.reply_text(f"**Your User ID:**\n`user_id`\n\n**You can click to copy and send it to @deathmatix if you want to buy a subscription.**", parse_mode=ParseMode.MARKDOWN)
 
 @app.on_callback_query(filters.regex("search_options"))
 async def search_options_callback_handler(client, query: CallbackQuery):
@@ -79,12 +78,15 @@ async def search_type_callback_handler(client, query: CallbackQuery):
     await query.answer()
     search_type = query.data.replace("search_type_", "")
     user_choices[query.from_user.id] = search_type
-    await query.message.reply_text(f"**Please enter the {search_type} you want to search for (Reply to this message).**", parse_mode=ParseMode.MARKDOWN)
+    await query.message.reply_text(f"**Please enter the search_type you want to search for (Reply to this message).**", parse_mode=ParseMode.MARKDOWN)
 
 @app.on_message(filters.text & filters.reply)
 async def handle_search_query(client, message: Message):
     user_list = get_github_raw_file_content_as_list("https://raw.githubusercontent.com/Deathmatix/verify-logsearch/main/users.txt")
-    if str(message.from_user.id) in user_list:
+    user_usage = load_user_usage()
+    user_id = str(message.from_user.id)
+    
+    if str(message.from_user.id) in user_list or user_usage.get(user_id, 0) < 2:
         search_term = message.text
         sanitized_search_term = re.sub(r'[\\/:*?"<>|]', '_', search_term)
         await message.reply_text("**Fetching results...**", parse_mode=ParseMode.MARKDOWN)
@@ -109,7 +111,6 @@ async def handle_search_query(client, message: Message):
                     'Content-Type': 'application/json',
                 }
                 method = 'POST' if body else 'GET'
-
                 data = json.dumps(body) if body else None
                 response = requests.request(method, snusbase_api + url, headers=headers, data=data)
                 return response.json()
@@ -122,15 +123,31 @@ async def handle_search_query(client, message: Message):
             })  
             formatted_text = json.dumps(search_response, indent=2)
 
-        caption = f"Logs by @Deathmatix - {search_term} ({search_term.capitalize()})"
+        caption = f"Logs by @Deathmatix - {search_term.capitalize()}"
         formatted_text = caption + "\n\n" + formatted_text
-        result_file_name = f"{message.from_user.id}-{sanitized_search_term}.txt"
+        result_file_name = f"{message.from_user.id}_{sanitized_search_term}.txt"
+        
         with open(result_file_name, "w", encoding="utf-8") as text_file:
             text_file.write(formatted_text)
+        
+        if str(message.from_user.id) not in user_list:
+            with open(result_file_name, "r", encoding="utf-8") as text_file:
+                file_content = text_file.read()
+            truncated_content = file_content[:2048]
+            with open(result_file_name, "w", encoding="utf-8") as text_file:
+                text_file.write(truncated_content)
+        
         await message.reply_text("**Downloading/uploading file...**", parse_mode=ParseMode.MARKDOWN)
         await message.reply_document(result_file_name, caption=caption)
         os.remove(result_file_name)
+        
+        if str(message.from_user.id) not in user_list:
+            user_usage[user_id] = user_usage.get(user_id, 0) + 1
+            save_user_usage(user_usage)
+            remaining_searches = 2 - user_usage[user_id]
+            await message.reply_text(f"**You are a free user. You have used one of your free searches. You have {remaining_searches} searches remaining.**", parse_mode=ParseMode.MARKDOWN)
+            
     else:
-        await message.reply_text("**You are not a registered user. Contact @deathmatix to get the subscription.**", parse_mode=ParseMode.MARKDOWN)
+        await message.reply_text("**You have reached your limit. Please subscribe to continue using the bot. Contact @deathmatix to get the subscription.**", parse_mode=ParseMode.MARKDOWN)
 
 app.run()
